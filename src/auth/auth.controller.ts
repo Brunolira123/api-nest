106184dc10101bdce562e6f76e7fbe0dfadf6676
Bruno Lira <brunolira1@icloud.com>
2024-11-13 19:44:47 -0300

    Implemented JWT

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Body, Controller, Post } from "@nestjs/common";
+
+@Controller('auth')
+export class AuthController {
+
+    @Post('/login')
+    async login(@Body() body: AuthLoginDTO) {
+        return body
+    }
+
+}
