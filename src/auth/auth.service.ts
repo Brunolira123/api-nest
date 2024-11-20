48c3fced0516a56b048dde189a0b6ba1b4f2d49e
Bruno Lira <brunolira1@icloud.com>
2024-11-20 12:42:16 -0300

    Implemented login router

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class AuthService {
 
-    constructor (private readonly JWTService : JwtService) {}
+    constructor (
+        private readonly JWTService : JwtService,
+        private readonly prisma : PrismaService
+    ) {}
 
     async createToken(){
         return this.JWTService.sign({});
@@ -13,4 +17,52 @@ export class AuthService {
     async checkToken(token : string){
         return this.JWTService.verify(token);
     }
+
+    async login(email : string , password : string){
+
+        const user = await this.prisma.user.findFirst({
+            where: {
+                email,
+                password
+            }
+        });
+
+        if (!user) {
+            throw new UnauthorizedException('Credenciais inválidas');
+        }
+
+        return user;
+    }
+
+    async forget(email: string){
+        const user = await this.prisma.user.findFirst({
+            where: {
+                email
+            }  
+        });
+        if(!user){
+            throw new UnauthorizedException('Credenciais inválidas');
+        }
+
+        //to do enviar e-mail
+
+        return true;
+    }
+
+    async reset(password: string, token: string){
+        //TO DO Se o token for válido, atualizar a senha
+
+        const id = 0
+
+        await this.prisma.user.update({
+            where: {
+                id
+            },
+            data: {
+                password
+            }
+        });
+
+        return true
+    } 
 }
--- src/auth/dto/auth.login.dto.ts
+++ src/auth/dto/auth.login.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, IsString, MinLength } from "class-validator";
 
-export class AuthLoginDTO {
+export class  AuthLoginDTO {
 
     @IsEmail ()
     email: string;
