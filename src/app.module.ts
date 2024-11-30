5922bce4a81f2a08ded9b8978f07367f64764392
Brunolira123 <brunolira1@icloud.com>
2024-11-30 16:33:55 -0300

    Implemented conection to bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { UserIdCheckMiddleware } from './middlewares/user-id-check-middleware';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
